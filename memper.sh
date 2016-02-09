a046eebe6fc24d36837f697e8c6c223ef7e1555a
Alvaro D <blzrx@icloud.com>
2016-02-09 10:37:04 -0300

    Update memper.sh

--- memper.sh
+++ memper.sh
@@ -2,6 +2,7 @@ echo ""
 echo "************************************"
 echo    $USER on $HOSTNAME
 echo    $(date)
+echo    SHELL LEVEL: $SHLVL
 echo "************************************"
 echo ""
 echo "Physical memory usage on \"$HOSTNAME\": $((`free -m | grep 'Mem:' | awk {'print $3'}`*100/`free -m | grep 'Mem:' | awk {'print $2'}`))% (`free -m | grep 'Mem:' | awk {'print $4'}`MB) of `free -m | grep 'Mem:' | awk {'print $2'}`MB free"
